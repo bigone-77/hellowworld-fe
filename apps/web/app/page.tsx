36c809e0b73213abe9682b6746056b667d7acf38
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2025-07-21 04:29:01 +0900

    ⚡️ [fix] shared-styles @source로 스타일 import 하는 부분 적재

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,10 @@
 import Button from '@repo/ui/components/Button';
-import Input from '@repo/ui/components/Input';
 
 export default function Page() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <p>hi</p>
+      <p className='text-primary-box'>hi</p>
       <Button />
-      <Input />
     </main>
   );
 }
