fe763f93390069d0c1268bb14db112a77047374f
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2025-07-21 04:31:43 +0900

    🔥 [remove] 불필요한 css import 제거

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <p>docs</p>
+      <p className='text-primary-box'>docs</p>
     </main>
   );
 }
