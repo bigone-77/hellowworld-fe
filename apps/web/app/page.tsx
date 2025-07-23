fb45a0c10e447e556f2a60bcdd0447cb57a4a4d4
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2025-07-24 01:04:17 +0900

    🔧 [setting] tw-merge 중복 css 제거  variants 자유도 고려

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -7,18 +7,16 @@ export default function Page() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-16'>
       <p className='text-primary-box'>hi</p>
-      <Button variant='primary' size='xs'>
-        <InlineSvg
-          srcUrl='https://res.cloudinary.com/dl31hx4rn/image/upload/v1753116454/svg/star.svg'
-          className='text-yellow-30'
-        />
+      <Button variant='primary' size='xs' disabled>
+        <InlineSvg srcUrl='https://res.cloudinary.com/dl31hx4rn/image/upload/v1753271887/svg/star.svg' />
         버튼
       </Button>
       <Button variant='secondary' size='xl'>
         버튼
       </Button>
       <Button variant='text'>버튼</Button>
-      <Button variant='outline'>버튼</Button>
+      <Button variant='outline_m'>버튼</Button>
+      <Button variant='outline_s'>버튼</Button>
       <Button variant='secondary_icon' size='s'>
         <InlineSvg
           srcUrl='https://res.cloudinary.com/dl31hx4rn/image/upload/v1753116454/svg/star.svg'
--- packages/ui/src/components/Button.tsx
+++ packages/ui/src/components/Button.tsx
@@ -1,4 +1,5 @@
 import { ButtonHTMLAttributes, ReactNode } from 'react';
+import { cn } from '../lib/utils';
 
 // Props 타입 정의
 type ButtonVariant =
@@ -6,13 +7,15 @@ type ButtonVariant =
   | 'secondary'
   | 'primary_icon'
   | 'secondary_icon'
-  | 'outline'
+  | 'outline_s'
+  | 'outline_m'
   | 'text';
 type ButtonSize = 'xl' | 'l' | 'm' | 's' | 'xs';
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: ButtonVariant;
   size?: ButtonSize;
+  className?: string;
   children: ReactNode;
 }
 
@@ -20,11 +23,11 @@ const Button = ({
   variant = 'primary',
   size = 'm',
   children,
+  className: propsClassName,
   ...props
 }: ButtonProps) => {
-  // 기본 스타일: 모든 버튼에 공통 적용
   const baseStyle =
-    'inline-flex items-center justify-center gap-1 cursor-pointer shrink-0 border-2 whitespace-nowrap rounded-m transition-colors';
+    'inline-flex items-center justify-center gap-1 cursor-pointer shrink-0 border-2 whitespace-nowrap rounded-M transition-colors';
 
   const styles: Record<
     ButtonVariant,
@@ -33,15 +36,15 @@ const Button = ({
     primary: {
       base: `
           text-primary-on bg-primary-box border-primary-line shadow-[0_6px_0_0_var(--color-primary-line)] btn-press-effect
-          hover:bg-primary-box-var2 
-          focus:border-primary-on 
+          hover:bg-primary-box-var2
+          focus:border-primary-on  
           active:bg-primary-box-var3 active:text-primary-on active:border-primary-line active:shadow-[0_2px_0_0_var(--color-primary-line)]
-          disabled:bg-primary-box-var3 disabled:text-primary-on disabled:border-primary-line disabled:shadow-[0_2px_0_0_var(--color-primary-line)]
+          disabled:bg-text-box disabled:text-text2/30 disabled:border-text-line disabled:shadow-[0_2px_0_0_var(--color-text-line)] disabled:cursor-default
         `,
       xl: 'px-[375] py-3 text-title-l',
       l: 'px-[161] py-3 text-title-l',
       m: 'px-[70] py-3 text-title-l',
-      xs: 'px-3 py-[5] text-body-l2 !gap-0',
+      xs: 'px-3 py-[5] text-body-l2 gap-0',
     },
     secondary: {
       base: `
@@ -49,50 +52,60 @@ const Button = ({
           hover:bg-secondary-box-var1 
           focus:border-secondary-box-on 
           active:bg-secondary-box-var2 active:text-secondary-box-on active:border-secondary-line active:shadow-[0_2px_0_0_var(--color-secondary-line)]
-          disabled:bg-secondary-box-var2 active:text-secondary-box-on active:border-secondary-line active:shadow-[0_2px_0_0_var(--color-secondary-line)]
+          disabled:bg-text-box disabled:text-text2/30 disabled:border-text-line disabled:shadow-[0_2px_0_0_var(--color-text-line)] disabled:cursor-default
         `,
       xl: 'px-[375] py-3 text-title-l',
       l: 'px-[161] py-3 text-title-l',
       m: 'px-[70] py-3 text-title-l',
-      xs: 'px-3 py-[5] text-body-l2 !gap-0',
+      xs: 'px-3 py-[5] text-body-l2 gap-0',
     },
     primary_icon: {
       base: `
-          bg-primary-box border-primary-line !rounded-icon hover:bg-primary-box-var2 shadow-[0_2px_0_0_var(--color-primary-line)] btn-press-effect
+          bg-primary-box border-primary-line rounded-S btn-press-effect
+          hover:bg-primary-box-var2 shadow-[0_2px_0_0_var(--color-primary-line)]
           focus:border-primary-on 
           active:bg-primary-box-var3 active:text-primary-on active:border-primary-line active:shadow-[0_0px_0_0_var(--color-primary-line)]
-          disabled:bg-primary-box-var3 disabled:text-primary-on disabled:border-primary-line disabled:shadow-[0_0px_0_0_var(--color-primary-line)]
+          disabled:bg-primary-box-var3 disabled:text-primary-on disabled:border-primary-line disabled:shadow-[0_0px_0_0_var(--color-primary-line)] disabled:cursor-default
         `,
       m: 'w-14 h-14',
       s: 'w-11 h-11',
     },
     secondary_icon: {
       base: `
-          bg-secondary-box border-secondary-line !rounded-icon shadow-[0_2px_0_0_var(--color-primary-line)] btn-press-effect
+          bg-secondary-box border-secondary-line rounded-S shadow-[0_2px_0_0_var(--color-primary-line)] btn-press-effect
           hover:bg-secondary-box-var1
           focus:border-secondary-box-on 
           active:bg-secondary-box-var2 active:text-secondary-box-on active:border-secondary-line active:shadow-[0_2px_0_0_var(--color-secondary-line)]
-          disabled:bg-secondary-box-var2 active:text-secondary-box-on active:border-secondary-line active:shadow-[0_2px_0_0_var(--color-secondary-line)]
+          disabled:bg-secondary-box-var2 active:text-secondary-box-on active:border-secondary-line active:shadow-[0_2px_0_0_var(--color-secondary-line)] disabled:cursor-default
         `,
       m: 'w-14 h-14',
       s: 'w-11 h-11',
     },
-    outline: {
+    outline_s: {
       base: `
         border-text-box-var text-body-s1 text-text1 px-3 py-[6] 
         hover:bg-text-box 
         focus:border-text1 
         active:bg-text-box-var active:border-text-line
-        disabled:bg-text-box-var active:border-text-line
+        disabled:bg-text-box-var disabled:border-text-line disabled:cursor-default
+      `,
+    },
+    outline_m: {
+      base: `
+        border-text-box-var text-body-l2 text-text1 px-3 py-[6] 
+        hover:bg-text-box 
+        focus:border-text1 
+        active:bg-text-box-var active:border-text-line
+        disabled:bg-text-box-var disabled:border-text-line disabled:cursor-default
       `,
     },
     text: {
       base: `
         border-none text-body-s1 text-text1 px-4 py-[6] 
         hover:bg-text-box 
-        focus:border-text1 focus:bg-text-box 
+        focus:border-solid focus:border-text1 focus:border-2 focus:bg-text-box
         active:bg-text-box-var
-        disabled:bg-text-box-var
+        disabled:bg-text-box disabled:text-text1 disabled:cursor-default
       `,
     },
   };
@@ -100,10 +113,10 @@ const Button = ({
   const variantStyle = styles[variant]?.base || '';
   const sizeStyle = styles[variant]?.[size] || '';
 
-  const className = `${baseStyle} ${variantStyle} ${sizeStyle}`;
+  const finalClassName = cn(baseStyle, variantStyle, sizeStyle, propsClassName);
 
   return (
-    <button className={className.trim()} {...props}>
+    <button className={finalClassName.trim()} {...props}>
       {children}
     </button>
   );
