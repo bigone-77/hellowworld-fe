42a9b0e9209de524f32546dbd0ec22e07f36a67d
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2025-08-02 19:24:36 +0900

    🎨 [publish] Progressbar 공통 컴포넌트 구현

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,55 @@
-import Label from '@repo/ui/components/Label';
+'use client';
+
+import Progressbar from '@repo/ui/components/Progressbar';
+import { useState } from 'react';
 
 export default function Page() {
+  const [progress, setProgress] = useState(32);
   return (
     <main>
-      <Label>라벨</Label>
+      <div className='space-y-4 p-10'>
+        <Progressbar progress={progress} />
+        <div className='flex gap-2'>
+          <button
+            onClick={() => setProgress(1)}
+            className='rounded bg-gray-200 p-2'
+          >
+            1%
+          </button>
+          <button
+            onClick={() => setProgress(5)}
+            className='rounded bg-gray-200 p-2'
+          >
+            5%
+          </button>
+          <button
+            onClick={() => setProgress(32)}
+            className='rounded bg-gray-200 p-2'
+          >
+            32%
+          </button>
+          <button
+            onClick={() => setProgress(97)}
+            className='rounded bg-gray-200 p-2'
+          >
+            97%
+          </button>
+          <button
+            onClick={() => setProgress(100)}
+            className='rounded bg-gray-200 p-2'
+          >
+            100%
+          </button>
+        </div>
+        <input
+          type='range'
+          min='0'
+          max='100'
+          value={progress}
+          onChange={(e) => setProgress(Number(e.target.value))}
+          className='w-full'
+        />
+      </div>
     </main>
   );
 }
